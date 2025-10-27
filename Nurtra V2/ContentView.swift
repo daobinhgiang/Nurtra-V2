0cd4b626ed40d14a7d3eef2181a06832a4b3f2e2
Giang Dao <giang.bdao@gmail.com>
2025-10-27 12:18:04 -0700

    Initial Commit

---
+++ Nurtra V2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nurtra V2
+//
+//  Created by Giang Michael Dao on 10/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nurtra V2/Nurtra_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nurtra_V2App.swift
+//  Nurtra V2
+//
+//  Created by Giang Michael Dao on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Nurtra_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
